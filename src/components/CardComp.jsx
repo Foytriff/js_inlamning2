d0f49d699a2bd3bea8e655ce23b656df2583dee2
foytriff <gustavhf73@gmail.com>
2021-11-25 18:05:31 +0100

    card comp inprog

---
+++ src/components/CardComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CardComp(myDeck) {
+
+    
+    
+
+    return (
+        <div>
+            <button><img src={`${myDeck.image}`} /></button>
+        </div>
+    )
+}
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,29 +1,32 @@
-import React, { useState } from 'react'
-
-const [state, setState] = useState([]);
+import React, { useState, useEffect } from 'react'
 
 function card(cardData) {
     console.log(cardData);
     return <button>{cardData}</button>
 }
 
-const getDeck = async () =>{
-    const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-    const data =  await res.json();
-    let myDeck = [];
-    for (let i = 0; i < 20; i++){
-        const res2 = await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`);
-        const data2 = await res2.json();
-        myDeck[i] = data2;
-    }
-    setState(myDeck);
-}
+export default function GameBoard() {
 
-export default function gameBoard() {
+    let myDeck = [];
+
+    const [state, setState] = useState([]);
+
+    useEffect(async () => {
+        const res = await fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+        const data =  await res.json();
+        
+        for (let i = 0; i < 20; i++){
+            const res2 = await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`);
+            const data2 = await res2.json();
+            console.log(data2);
+            myDeck[i] = data2;
+        }
+        setState(myDeck);
+    }, [])
     
     return (
         <div>
-            {getDeck().then((val) => val.map(data => card(data)))}
+            
         </div>
     )
 }
